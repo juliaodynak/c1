eb11348c3887204d9edaaf47d424cbef90bf0411
adminaccount <adminaccount@macmini-4.training.local>
2015-08-21 16:34:09 +0300

    Initial Commit

---
+++ c1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  c1
+//
+//  Created by adminaccount on 8/21/15.
+//  Copyright (c) 2015 pelekh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
